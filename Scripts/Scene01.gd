34e6e049a493f77f3e7779de3410721435811b84
Arie <arieinjector@gmail.com>
2023-03-08 09:47:00 +0700

    learn movement char & physics

--- Scripts/Scene01.gd
+++ Scripts/Scene01.gd
@@ -1,26 +1,7 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var count = 0
-var speed = 5
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 	
 
-func _input(event):
-	print(event)
 
-func _process(delta):
-	if (Input.is_action_just_pressed("Maju")) :
-		$Zombie.position.x += speed
-		$Zombie.rotation_degrees += 5
-	elif (Input.is_action_just_pressed("Mundur")) :
-		$Zombie.position.x -= speed
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
---
+++ ZombieMovement.gd
@@ -0,0 +1,43 @@
+extends KinematicBody2D
+
+export var speed = 100
+var movement = Vector2(0,0)
+const accel = 50
+const max_speed = 100
+const gravity = 10
+const jump_force = -300
+const max_jump = 2
+var jump_count = 0
+
+func _physics_process(delta):
+	
+	if (not is_on_floor()) :
+		movement.y += gravity
+	else :
+		movement.y = 0
+		jump_count = 0
+		movement.x = lerp(movement.x, 0, 0.1)
+
+	if (Input.is_action_pressed("Maju")) :
+		movement.x += accel
+		movement.x = min(movement.x, max_speed)
+	elif (Input.is_action_pressed("Mundur")) :
+		movement.x -= accel
+		movement.x = max(movement.x, -max_speed)
+	
+	if (Input.is_action_pressed("Atas")) :
+		movement.y -= accel
+		movement.y = max(movement.y, -max_speed)
+	elif (Input.is_action_pressed("Bawah")) :
+		movement.y += accel
+		movement.y = min(movement.y, max_speed)
+		
+	if (Input.is_action_just_pressed("Lompat") and ( is_on_floor() or (jump_count < max_jump) ) ) :
+		movement.y = jump_force
+		jump_count += 1
+	elif (is_on_ceiling()) :
+		movement.y = 0
+	elif (is_on_wall()) :
+		print('Njir kamu nabrak tembok')
+		
+	move_and_slide(movement, Vector2(0, -1))
